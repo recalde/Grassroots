ccc374206390c848faee57c3c1130d72f93a1d51
Robert Recalde <recalde@gmail.com>
2011-10-04 20:59:15 -0400

    Initialized commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grassroots::Application.config.secret_token = '5edc9bbf9c480dc1db79e2cce756820399450560b08acdffc86310ccd335897c6b2e87738cfbae92aeb25116c053ea267e1436cf719471b4e4b889d196a620ac'
