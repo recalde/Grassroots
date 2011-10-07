f94721043d56e72a776b56d55b4b237d17aee66d
Robert Recalde <recalde@gmail.com>
2011-10-07 14:36:14 -0400

    Bundled/Raked Devise and added home link

--- app/controllers/grassroots_controller.rb
+++ app/controllers/grassroots_controller.rb
@@ -9,23 +9,24 @@ class GrassrootsController < ApplicationController
   # GET /vote
   # GET /vote.json
   def vote
+    if user_signed_in?
+      @idea = Idea.find(params[:idea_id])
+      @vote = params[:vote]
     
-    @idea = Idea.find(params[:idea_id])
-    @vote = params[:vote]
+      if @vote == "yae"
+        @idea.yae_votes+=1
+      else 
+        @idea.nae_votes+=1
+      end
     
-    if @vote == "yae"
-      @idea.yae_votes+=1
-    else 
-      @idea.nae_votes+=1
-    end
-    
-    @idea.rank = @idea.calculate_rank
+      @idea.rank = @idea.calculate_rank
 
-    respond_to do |format|
-      if @idea.save
-        format.json { head :ok }
-      else
-        format.json { render :json => @idea.errors, :status => :unprocessable_entity }
+      respond_to do |format|
+        if @idea.save
+          format.json { head :ok }
+        else
+          format.json { render :json => @idea.errors, :status => :unprocessable_entity }
+        end
       end
     end
   end
@@ -33,21 +34,23 @@ class GrassrootsController < ApplicationController
   # GET /new-idea
   # GET /new-idea.json
   def new_idea
-    @idea = Idea.new()
-    @idea.subject = params[:new_idea_subject]
-    @idea.description = params[:new_idea_description]
-    @idea.category_id = params[:new_idea_category_id]
-    @idea.yae_votes = 1
-    @idea.nae_votes = 0
-    @idea.rank = 1
-    @idea.user_id = current_user.id
-    respond_to do |format|
-      if @idea.save
-        format.html { redirect_to @idea, :notice => 'Idea was successfully created.' }
-        format.json { render :json => @idea, :status => :created, :location => @idea }
-      else
-        format.html { render :action => "new" }
-        format.json { render :json => @idea.errors, :status => :unprocessable_entity }
+    if user_signed_in?
+      @idea = Idea.new()
+      @idea.subject = params[:new_idea_subject]
+      @idea.description = params[:new_idea_description]
+      @idea.category_id = params[:new_idea_category_id]
+      @idea.yae_votes = 1
+      @idea.nae_votes = 0
+      @idea.rank = 1
+      @idea.user_id = current_user.id
+      respond_to do |format|
+        if @idea.save
+          format.html { redirect_to @idea, :notice => 'Idea was successfully created.' }
+          format.json { render :json => @idea, :status => :created, :location => @idea }
+        else
+          format.html { render :action => "new" }
+          format.json { render :json => @idea.errors, :status => :unprocessable_entity }
+        end
       end
     end
   end
